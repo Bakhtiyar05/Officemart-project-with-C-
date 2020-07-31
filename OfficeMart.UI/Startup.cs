65dffa583d357d650c74ce4346f3b5fcef04f70a
mehdizadeafiq@gmail.com <mehdizadeafiq@gmail.com>
2020-07-31 20:31:45 +0400

    Add Admin Panel Home Page

--- OfficeMart.UI/Startup.cs
+++ OfficeMart.UI/Startup.cs
@@ -46,8 +46,13 @@ namespace OfficeMart.UI
 
             app.UseAuthorization();
 
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                      name: "areas",
+                      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
