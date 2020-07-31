7fa6b33c920e2814a8d76c9f8bcf8aed0fcac817
Vusal Nabiyev <vusalkhn@code.edu.az>
2020-07-31 13:27:49 +0400

    removed unuse referance

--- OfficeMart.UI/Controllers/HomeController.cs
+++ OfficeMart.UI/Controllers/HomeController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using OfficeMart.Business.Logic;
 using OfficeMart.UI.Models;
+using System.Diagnostics;
 
 namespace OfficeMart.UI.Controllers
 {
