ba69b2dbddf1ab4a00c1f310ad144bf9524b6f24
mehdizadeafiq@gmail.com <mehdizadeafiq@gmail.com>
2020-07-31 22:14:48 +0400

    product add form added

---
+++ OfficeMart.UI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OfficeMart.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
