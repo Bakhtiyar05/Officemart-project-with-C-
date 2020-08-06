4b7334eebb26d5e9e4748f54a965cf250a73f8cb
mehdizadeafiq@gmail.com <mehdizadeafiq@gmail.com>
2020-08-06 23:35:35 +0400

    products added to to products view

---
+++ OfficeMart.UI/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OfficeMart.Business.Logic;
+
+namespace OfficeMart.UI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var products = await new ProductLogic().GetProducts();
+            return View(products);
+        }
+    }
+}
