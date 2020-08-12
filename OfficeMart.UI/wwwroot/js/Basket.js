c06f63b3ecacc35fcc577acca20cfb70505a3f3e
mehdizadeafiq@gmail.com <mehdizadeafiq@gmail.com>
2020-08-13 01:37:02 +0400

    Basket Doned!

--- OfficeMart.UI/wwwroot/js/Basket.js
+++ OfficeMart.UI/wwwroot/js/Basket.js
@@ -3,14 +3,43 @@
     $(document).on("click", ".prod-addbtn", function (e) {
         e.preventDefault();
         var clickedId = $(this).attr("id");
-        var imgSRC = $(`#${clickedId}`).attr("src");
+        if ($(".cart_list li").length > 0) {
+            var isDuplicate = false;
+            $('.cart_list').children('li').each(function () {
+                var currentLiId = $(this).attr('id');
+
+                if (currentLiId == `basketed-li-${clickedId}`) {
+                    $(this).remove();
+                    appendBask(clickedId);
+                    isDuplicate = true;
+                    return false;
+                }
+            });
+            if (!isDuplicate) {
+                appendBask(clickedId);
+            }
+        } else {
+            appendBask(clickedId);
+        }
+        getkBasketCount();
+    });
+
+    $(document).on("click", ".remove", function (e) {
+        e.preventDefault();
+        var clickedId = $(this).attr("del-id");
+        $("li").remove(`#basketed-li-${clickedId}`);
+        getkBasketCount();
+    });
+
+    function appendBask(clickedId) {
+        var imgSRC = $(`#baseimage-${clickedId}`).attr("src");
         var productName = $(`#product-name-${clickedId}`).text();
         var totalPrice = $(`#total-value-${clickedId}`).val();
         var totalCount = $(`#count-${clickedId}`).val();
 
-        var element = `<li id="${clickedId}">
+        var element = `<li id="basketed-li-${clickedId}">
 
-                                        <a href="#" class="remove">&times;</a>
+                                        <a del-id="${clickedId}" class="remove">&times;</a>
 
                                         <a href="#">
 
@@ -25,6 +54,11 @@
                                     </li>`;
 
         $(".cart_list").append(element);
-    });
+    }
+
+    function getkBasketCount() {
+        var count = $(".cart_list li").length;
+        $("#basket-count").text(count);
+    }
 
 });
