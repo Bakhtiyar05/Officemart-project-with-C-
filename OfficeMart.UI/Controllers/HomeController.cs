8286e08dbf12a6525bf8513f495108631a2a6bff
Vusal Nabiyev <vusalkhn.rm@gmail.com>
2020-08-23 13:40:56 +0400

    localizer ready

--- OfficeMart.UI/Controllers/HomeController.cs
+++ OfficeMart.UI/Controllers/HomeController.cs
@@ -1,16 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using OfficeMart.Business.Logic;
+using System;
 using System.Threading.Tasks;
 
 namespace OfficeMart.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer<HomeController> _localizer;
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+        }
         public async Task<IActionResult> Index()
         {
+            ViewData["Message"] = _localizer["Your application description page."];
             var categories = await new HomeLogic().GetCategories();
             return View(categories);
         }
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
 
+            return LocalRedirect(returnUrl);
+        }
     }
 }
